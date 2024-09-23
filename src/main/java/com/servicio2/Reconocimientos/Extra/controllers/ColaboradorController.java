7fe9217bd21f21fc969e41c28c9df6fc46d81568
Fernando García <fernagarcia@frba.utn.edu.ar>
2024-09-23 12:13:02 -0300

    Cambio de POST por GET con RequestParam

--- src/main/java/com/servicio2/Reconocimientos/Extra/controllers/ColaboradorController.java
+++ src/main/java/com/servicio2/Reconocimientos/Extra/controllers/ColaboradorController.java
@@ -1,6 +1,7 @@
 package com.servicio2.Reconocimientos.Extra.controllers;
 
 import com.servicio2.Reconocimientos.Extra.DTOs.DTOActualizarViandas;
+import com.servicio2.Reconocimientos.Extra.models.Colaborador;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -8,7 +9,6 @@ import com.servicio2.Reconocimientos.Extra.services.ColaboradorService;
 import com.servicio2.Reconocimientos.Extra.DTOs.DTOColaboradorSolicitado;
 import com.servicio2.Reconocimientos.Extra.DTOs.DTOConsulta;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,23 +16,32 @@ import java.util.List;
 @RestController
 @RequestMapping
 public class ColaboradorController {
-    @Autowired
-    ColaboradorService colaboradorService;
+    private final ColaboradorService colaboradorService;
 
-    @Operation(summary = "Pantalla inicio", description = "Nombre de la API")
-    @GetMapping("")
-    public String inicio(){
-        return "API - Servicio 2 - Reconocimiento Extra";
+    public ColaboradorController(ColaboradorService colaboradorService) {
+        this.colaboradorService = colaboradorService;
     }
 
-    @Operation(summary = "Obtener lista de colaboradores", description = "Retorna una lista de colaboradores filtrados "
+    @Operation(summary = "Mostrar todos los colaboradores", description = "Todos los colaboradores")
+    @ApiResponse(responseCode = "200", description = "Lista de colaboradores",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Colaborador.class)))
+    @GetMapping
+    public List<Colaborador> inicio(){
+        return colaboradorService.obtenerTodos();
+    }
+
+    @Operation(summary = "Obtener lista de colaboradores en base a parametros",
+            description = "Retorna una lista de colaboradores filtrados "
             + "por cantidad de donaciones el ultimo mes y puntos obtenidos históricamente")
     @ApiResponse(responseCode = "200", description = "Lista de colaboradores",
                     content = @Content(mediaType = "application/json",
                     schema = @Schema(implementation = DTOColaboradorSolicitado.class)))
-    @PostMapping("/colaboradores")
-    public List<DTOColaboradorSolicitado> obtenerColaboradores(@RequestBody DTOConsulta consulta){
-        return colaboradorService.obtenerColaboradores(consulta);
+    @GetMapping("/colaboradores")
+    public List<DTOColaboradorSolicitado> obtenerColaboradores(@RequestParam Integer minPuntos,
+                                                               @RequestParam Integer minDonacionUltMes,
+                                                               @RequestParam Integer cantidadMaxADevolver){
+        return colaboradorService.obtenerColaboradores(minPuntos, minDonacionUltMes, cantidadMaxADevolver);
     }
 
     @Operation(summary = "Actualizar la cantidad de viandas donadas el ultimo mes de un colaborador")
--- src/main/java/com/servicio2/Reconocimientos/Extra/services/ColaboradorService.java
+++ src/main/java/com/servicio2/Reconocimientos/Extra/services/ColaboradorService.java
@@ -3,6 +3,7 @@ package com.servicio2.Reconocimientos.Extra.services;
 import com.servicio2.Reconocimientos.Extra.DTOs.DTOActualizarViandas;
 import com.servicio2.Reconocimientos.Extra.DTOs.DTOColaboradorSolicitado;
 import com.servicio2.Reconocimientos.Extra.DTOs.DTOConsulta;
+import com.servicio2.Reconocimientos.Extra.models.Colaborador;
 import com.servicio2.Reconocimientos.Extra.repositories.ColaboradorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
@@ -15,16 +16,23 @@ import static java.lang.Double.valueOf;
 
 @Service
 public class ColaboradorService {
-    @Autowired
-    ColaboradorRepository colaboradorRepository;
+    private final ColaboradorRepository colaboradorRepository;
 
-    public List<DTOColaboradorSolicitado> obtenerColaboradores(DTOConsulta consulta){
-        Pageable pageable = PageRequest.of(0, consulta.getCantidadMaxADevolver());
+    public ColaboradorService(ColaboradorRepository colaboradorRepository) {
+        this.colaboradorRepository = colaboradorRepository;
+    }
 
-        return colaboradorRepository.findAllByPuntajeAndDonaciones(valueOf(consulta.getMinPuntos()), consulta.getMinDonacionUltMes(), pageable);
+    public List<DTOColaboradorSolicitado> obtenerColaboradores(Integer minPuntos, Integer minViandas, Integer cantidad){
+        Pageable pageable = PageRequest.of(0, cantidad);
+
+        return colaboradorRepository.findAllByPuntajeAndDonaciones(valueOf(minPuntos), minViandas, pageable);
     }
 
     public int actualizarColaborador(DTOActualizarViandas actualizacion) {
         return colaboradorRepository.updateViandasColaboradorById(actualizacion.getId_colaborador(), actualizacion.getCantidad_viandas());
+    }
+
+    public List<Colaborador> obtenerTodos(){
+        return colaboradorRepository.findAll();
     }
 }
